b24f8b3ddbb892e7ee646f195c9bc0a734475746
Kamrujjaman Ali <kamrujjamanali2@gmail.com>
2025-02-19 11:35:30 +0530

    changes

--- src/app/layout/patient/patient-routing.module.ts
+++ src/app/layout/patient/patient-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PatientComponent } from './patient.component';
+import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
 
-const routes: Routes = [{ path: '', component: PatientComponent }];
+const routes: Routes = [
+  { path: '', component: PatientComponent },
+  { path: 'dashboard', component: PatientDashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/layout/patient/patient.module.ts
+++ src/app/layout/patient/patient.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PatientRoutingModule } from './patient-routing.module';
 import { PatientComponent } from './patient.component';
+import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
 
 
 @NgModule({
   declarations: [
-    PatientComponent
+    PatientComponent,
+    PatientDashboardComponent
   ],
   imports: [
     CommonModule,
