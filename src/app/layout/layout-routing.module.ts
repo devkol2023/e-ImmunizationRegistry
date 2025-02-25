913d435989208a8cdbe30c04a90fe389c8a02c01
Kamrujjaman Ali <kamrujjamanali2@gmail.com>
2025-02-25 18:13:01 +0530

    calender implementation

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: '', component: LayoutComponent },
   { path: 'patient', loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule) },
   { path: 'health-worker', loadChildren: () => import('./health-worker/health-worker.module').then(m => m.HealthWorkerModule) },
+  { path: 'calender', loadChildren: () => import('./calender/calender.module').then(m => m.CalenderModule) },
 ];
 
 @NgModule({
